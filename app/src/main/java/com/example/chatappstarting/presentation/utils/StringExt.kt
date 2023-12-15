6dd5023b0e74e016e970829cafc81ad93dd9e878
fahimShahriar <f.sm151496@gmail.com>
2023-12-16 00:50:06 +0600

    Extension added.

--- app/src/main/java/com/example/chatappstarting/presentation/utils/StringExt.kt
+++ app/src/main/java/com/example/chatappstarting/presentation/utils/StringExt.kt
@@ -6,3 +6,19 @@ fun String?.emptyIfNull(): String {
     else
         this
 }
+
+fun String.isPhoneOperatorValid(): Boolean {
+    return startsWith("013") ||
+            startsWith("014") ||
+            startsWith("015") ||
+            startsWith("016") ||
+            startsWith("017") ||
+            startsWith("018") ||
+            startsWith("019")
+}
+
+fun String?.isPhoneNumberValid() =
+    if (isNullOrEmpty())
+        false
+    else
+        length == 11 && isPhoneOperatorValid()
