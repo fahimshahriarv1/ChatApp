8f1270b9d8c3ba39999eb5630443aa97240fa27d
fahimShahriar <f.sm151496@gmail.com>
2023-12-16 00:55:31 +0600

    view model added.

---
+++ app/src/main/java/com/example/chatappstarting/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.chatappstarting.presentation.ui.home
+
+import com.example.chatappstarting.presentation.ui.base.BaseViewModel
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+}
