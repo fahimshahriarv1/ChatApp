2fa0379c85b82c6b02f585855aa811afaaf61c1c
fahimShahriar <f.sm151496@gmail.com>
2024-09-22 18:45:56 +0600

    UseCase modified.

--- app/src/main/java/com/example/chatappstarting/domain/usecases/GetConnectedUsersUseCase.kt
+++ app/src/main/java/com/example/chatappstarting/domain/usecases/GetConnectedUsersUseCase.kt
@@ -1,11 +1,11 @@
 package com.example.chatappstarting.domain.usecases
 
-import com.example.chatappstarting.domain.manager.LocalUserManger
+import com.example.chatappstarting.domain.manager.LocalDataBaseManger
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetConnectedUsersUseCase @Inject constructor(private val localUserManger: LocalUserManger) {
-    fun getConnectionList(): Flow<List<String>> {
-        return localUserManger.getConnectedList()
+class GetConnectedUsersUseCase @Inject constructor(private val localDataBaseManger: LocalDataBaseManger) {
+    fun getConnectionList(uName: String): Flow<List<String>> {
+        return localDataBaseManger.getConnectedList(uName)
     }
 }
