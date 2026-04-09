1fdd02ca65d0045510693c70b0ac454a7a678c0e
fahimshahriarv1 <f.sm151496@gmail.com>
2026-04-09 23:05:28 +0600

    Unused dependencies removed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,8 +92,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -120,14 +118,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.room:room-rxjava2:2.6.1")
-
-    //Ktor
-    implementation("io.ktor:ktor-client-core:2.3.1")
-    implementation("io.ktor:ktor-client-android:2.3.1")
-    implementation("io.ktor:ktor-client-gson:2.3.1")
-    implementation("io.ktor:ktor-serialization-gson:2.3.1")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.1")
 
     //firebaseAuth
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
